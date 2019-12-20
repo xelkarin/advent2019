478571dd7db5cebed7f47cc8b85a64afbb3d4d6c
Robert Gill <locke@sdf.lonestar.org>
2019-12-19 18:15:01 -0800

    day1: Solve part 2 using Ruby

--- day1/ruby/solve.rb
+++ day1/ruby/solve.rb
@@ -7,29 +7,41 @@ class FuelCalc
   def initialize(data)
     case data
     when String
-      @datafile = File.open(data, 'r')
+      @data = read_data(File.open(data, 'r'))
     when File
-      @datafile = data
+      @data = read_data(data)
+    when Array
+      @data = data
+    when Integer
+      @data = [data]
     else
-      raise TypeError, 'Expected a path name (String) or File'
+      raise TypeError, 'Expected a path name (String), File, or Integer data'
     end
-  end
-
-  def calc_fuel(mass)
-    (mass / 3) - 2
-  end
-
-  def sum_fuel
-    sum = 0
-    @datafile.each_line do |line|
-      mass = line.strip.to_i
-      sum += calc_fuel(mass)
-    end
-    sum
   end
 
   def required_fuel
     @required_fuel ||= sum_fuel
+  end
+
+  private
+
+  def read_data(datafile)
+    datafile.each_line.collect { |l| l.strip.to_i }
+  end
+
+  def calc_fuel(mass)
+    calc_fuel_r(mass, 0)
+  end
+
+  def calc_fuel_r(mass, total)
+    mass = mass / 3 - 2
+    return total if mass <= 0
+
+    calc_fuel_r(mass, total + mass)
+  end
+
+  def sum_fuel
+    @data.reduce(0) { |sum, mass| sum + calc_fuel(mass) }
   end
 end
 
